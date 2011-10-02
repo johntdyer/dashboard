9304dbf354320998ac1d81e4b554353e19406218
John Dyer <john@krumpt.com>
2011-10-02 16:08:02 -0400

    added datatables

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,5 +26,7 @@ Dashboard::Application.configure do
   config.assets.compress = false
 
   # Expands the lines which load the assets
-  config.assets.debug = true
+  config.assets.debug = true  
+  
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
