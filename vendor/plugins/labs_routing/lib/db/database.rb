8c00f9dca4b371177f9fe5ec2c70c7dc441973d4
John Dyer <john@krumpt.com>
2011-11-14 09:55:54 -0500

    removed tropo provisioning since we are no longer useing it.  Added links on the home page

--- vendor/plugins/labs_routing/lib/db/database.rb
+++ vendor/plugins/labs_routing/lib/db/database.rb
@@ -1,6 +1,4 @@
 module LabsRouting
-
   BASE_PATH = "config"
-  #Dir.mkdir(BASE_PATH) unless Dir.exist?(BASE_PATH)
   $config = LabsRouting::Config.new(File.open("#{LabsRouting::BASE_PATH}/config.yml") { |file| YAML.load(file) })
 end
--- vendor/plugins/tropo_provisioning/init.rb
+++
@@ -1,1 +0,0 @@
-require 'tropo_provisioning'
