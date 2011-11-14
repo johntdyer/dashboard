abcbb312bd6649a106b3b7cac527b682915362d8
John Dyer <john@krumpt.com>
2011-11-13 21:32:33 -0500

    moved config file into conf dir

--- vendor/plugins/labs_routing/lib/db/database.rb
+++ vendor/plugins/labs_routing/lib/db/database.rb
@@ -1,6 +1,6 @@
 module LabsRouting
 
-  BASE_PATH = "#{Dir.home()}/.lab_routing"
-  Dir.mkdir(BASE_PATH) unless Dir.exist?(BASE_PATH)
+  BASE_PATH = "config"
+  #Dir.mkdir(BASE_PATH) unless Dir.exist?(BASE_PATH)
   $config = LabsRouting::Config.new(File.open("#{LabsRouting::BASE_PATH}/config.yml") { |file| YAML.load(file) })
 end
