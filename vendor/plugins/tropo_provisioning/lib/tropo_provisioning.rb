9976d3e7b8ca2aea03a03f4ec32ad289abdd78ae
John Dyer <john@krumpt.com>
2011-11-14 09:55:54 -0500

    removed tropo provisioning since we are no longer useing it.  Added links on the home page

--- vendor/plugins/tropo_provisioning/lib/tropo_provisioning.rb
+++
@@ -1,7 +0,0 @@
-module TropoProvisioningConfig
-  def self.new(params={})
-    username = params[:username] || ENV['PROV_API_UNAME']
-    password = params[:password] || ENV['PROV_API_PASSWD']
-    base_uri = params[:base_uri] || ENV['PROV_API_BASE_URI']
-  end
-end
