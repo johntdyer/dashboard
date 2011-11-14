50c9968cfc0387a6034ca2390253ee488cc21168
John Dyer <john@krumpt.com>
2011-11-13 20:37:57 -0500

    cleanup of old code

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User
   include HTTParty
 
   def initialize(u,p)
-    @user_data = User.get("#{ENV['PROV_API_BASE_URI']}/user",:basic_auth => {:username => u, :password => p}).parsed_response
+    @user_data = User.get("#{$config.provisioning.api_endpoint}/user",:basic_auth => {:username => u, :password => p}).parsed_response
 
     if @user_data
       @user_data.merge!(:roles=>get_roles)
@@ -19,8 +19,8 @@ class User
   private
 
   def get_roles
-    auth = {:username => ENV['PROV_API_UNAME'], :password => ENV['PROV_API_PASSWD']}
-    User.get("#{ENV['PROV_API_BASE_URI']}/users/#{@user_data['username']}/roles",:basic_auth => auth).parsed_response.collect{|x| x["roleName"]}
+    auth = {:username => $config.provisioning.username, :password => $config.provisioning.password}
+    User.get("#{$config.provisioning.api_endpoint}/users/#{@user_data['username']}/roles",:basic_auth => auth).parsed_response.collect{|x| x["roleName"]}
   end
 
 end
--- vendor/plugins/labs_routing/lib/db/database.rb
+++ vendor/plugins/labs_routing/lib/db/database.rb
@@ -2,50 +2,5 @@ module LabsRouting
 
   BASE_PATH = "#{Dir.home()}/.lab_routing"
   Dir.mkdir(BASE_PATH) unless Dir.exist?(BASE_PATH)
-  ROUTING_DB = "#{BASE_PATH}/routing.db"
   $config = LabsRouting::Config.new(File.open("#{LabsRouting::BASE_PATH}/config.yml") { |file| YAML.load(file) })
- #  DB = SQLite3::Database.new(ROUTING_DB)
- # 
- #   ActiveRecord::Base.establish_connection(:adapter => 'sqlite3', :database => ROUTING_DB, :pool => 20)
- #   ActiveRecord::Base.logger = Logger.new("#{BASE_PATH}/debug.log", 10, 1024000)
- # 
- # #  require File.join File.dirname(__FILE__), 'initilize.rb'
- # 
- # if !Browser.table_exists?
- # 
- #    ActiveRecord::Base.connection.create_table(:browsers) do |t|
- #     t.column :hostname, :string
- #     t.column :network, :string
- #     t.column :datacenter, :string
- #     t.column :dedicated, :boolean, :default => false
- #     t.column :description, :string
- #     t.column :updated_at, :timestamp
- #     t.column :created_at, :timestamp
- #     t.column :customer, :string
- #     t.column :partitions, :string
- #   end
- # 
- #   ActiveRecord::Base.connection.create_table(:partitions) do |t|
- #     t.column :ppid, :integer
- #     t.column :channel, :string
- #     t.column :datacenter, :string
- #     t.column :description, :string
- #     t.column :updated_at, :timestamp
- #     t.column :created_at, :timestamp
- #     t.column :browser_id, :integer
- #   end
- #    
- #   ActiveRecord::Base.connection.create_table(:gateways) do |t|
- #     t.column :hostname, :string
- #     t.column :datacenter, :string
- #     t.column :description, :string
- #     t.column :updated_at, :timestamp
- #     t.column :created_at, :timestamp
- #   end 
- #   
- #   
- # end
- # 
- # 
- #   require File.join File.dirname(__FILE__), 'seed.rb'           
 end
